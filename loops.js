8c4a47edb32b30bdb75188b30d31f1ac38974249
Corbin Hall <hallcorbin92@gmail.com>
2020-08-09 09:17:51 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,5 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++)
+  array[i]
   return i === 0 || i > 1 ? `${array.push(`I am ${i} strange loops.`)}` : `${array.push(`I am 1 strange loop.`)`
 }
