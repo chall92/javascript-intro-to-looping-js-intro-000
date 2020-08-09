5ae0c89ab194046727dd8a2263df404f5e67accd
Corbin Hall <hallcorbin92@gmail.com>
2020-08-09 09:02:19 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop() {
   for (let i = 0; i < 25; i++)
-  return `array{"I am ${[i]} strange loop${i === 0 || i > 1? 's' : ''}."}`
+  `${array[i]}`
+  return `{"I am ${[i]} strange loop${i === 0 || i > 1? 's' : ''}."`
 
   }
